552b36a1978f3d10d65e65a04cdb5d8e2491a0a8
FernandoMarino <fernandomariosantos@gmail.com>
2025-12-15 03:57:04 -0800

    new version deployed on 2025-12-15

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,7 @@
 import { buttonVariants } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
+import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }){
     return (
