0889960e41fb47898984ef2b16e2fb93ad6431b1
FernandoMarino <fernandomariosantos@gmail.com>
2025-12-19 14:53:58 -0800

    corrigindo firebase.ts firebaseAdmin.ts e envs

--- app/api/(auth)/login/route.tsx
+++ app/api/(auth)/login/route.tsx
@@ -1,13 +1,28 @@
-
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
     try {
         console.log("Login API Route");
 
+        console.log(
+            "Private key length:",
+            process.env.FIREBASE_PRIVATE_KEY?.length
+        );
+        console.log(
+            "Private key starts with:",
+            process.env.FIREBASE_PRIVATE_KEY?.slice(0, 30)
+        );
+        console.log(
+            "Private key ends with:",
+            process.env.FIREBASE_PRIVATE_KEY?.slice(-30)
+        );
+
         const { email, password } = await request.json();
 
-        const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+        const apiKey = process.env.FIREBASE_API_KEY;
+
+        console.log("apiKey",apiKey);
+        
         const authUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
 
         const res = await fetch(authUrl, {
--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -10,12 +10,12 @@ import { FirebaseStorage, getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAtzaqOhR-y2H3vrlx5s6WNGuwPC_QxGD8",
-  authDomain: "my-trips-pro.firebaseapp.com",
-  projectId: "my-trips-pro",
-  storageBucket: "my-trips-pro.firebasestorage.app",
-  messagingSenderId: "833848740494",
-  appId: "1:833848740494:web:5d771d7260ba49028517df"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
